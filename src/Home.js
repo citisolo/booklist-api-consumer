19a14ad88d0fb67d8e2349210b20f3b7a1cdd3c9
kwadwo amankwa <citisolo@gmail.com>
2019-06-13 13:00:13 +0100

    lift booklist state up to home!

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
 import BookList from './components/BookList';
+import { connect } from 'react-redux';
+import { fetchBooks } from './actions';
 
 const HomeWrapper = styled.div`
     margin: 10em;
 `;
 
-const Home = () => (
-    <HomeWrapper>
-        <BookList/>
-    </HomeWrapper>
-)
 
-export default Home;
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: '1',
+        }
+    }
+
+    componentDidMount(){
+        const { dispatch } = this.props;
+        dispatch(fetchBooks({page: '1', itemsPerPage:'20'}));
+    }
+
+    handlePageClick = e => {
+        e.preventDefault();
+        const { dispatch } = this.props;
+        dispatch(fetchBooks({page: '1', itemsPerPage:'20'}));
+    }
+
+    render(){
+      const { isFetching, items } = this.props;
+
+      return (
+        <HomeWrapper>
+            <BookList isFetching={isFetching} items={items}/>
+        </HomeWrapper>
+      )
+    }
+  }
+
+
+const mapStateToProps = state => {
+  const { bookListReducer } = state;
+  console.log(state);
+  const { isFetching, items } = bookListReducer ;
+
+  return {
+    items,
+    isFetching,
+  }
+}
+
+export default connect(mapStateToProps)(Home);
+
+
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,72 +1,39 @@
   
   import React from 'react';
-  import { List, Segment, Dimmer, Loader, } from 'semantic-ui-react';
-  import { connect } from 'react-redux';
-  import { fetchBooks } from '../actions';
-  
-
-  class BookList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            page: '1',
-        }
-    }
-
-    componentDidMount(){
-        const { dispatch } = this.props;
-        dispatch(fetchBooks({page: '1', itemsPerPage:'20'}));
-    }
-
-    handlePageClick = e => {
-        e.preventDefault();
-        const { dispatch } = this.props;
-        dispatch(fetchBooks({page: '1', itemsPerPage:'20'}));
-    }
-
-    render(){
-      const { isFetching, items } = this.props;
-
-      return (
-        <Segment inverted>
-        <List divided inverted relaxed>
-            { isFetching ? (
-            <List.Item>
-              <Dimmer active>
-                <Loader />
-              </Dimmer>               
-            </List.Item>
-            ) : (
-                items.books.map(item => {
-                    return (
-                    <List.Item>
-                        <List.Content>
-                          <List.Header>{item.book_title}</List.Header>
-                          {item.book_author[0]}
-                        </List.Content>
-                    </List.Item> 
-                    )
-                })
-            )}
-        </List>
-      </Segment> 
-      )
-    }
-  }
+  import { List, Segment, Dimmer, Loader, Button } from 'semantic-ui-react';
 
 
-const mapStateToProps = state => {
-  const { bookListReducer } = state;
-  console.log(state);
-  const { isFetching, items } = bookListReducer ;
+  const BookList = ({isFetching, items}) => {
+    return (
+      <React.Fragment>
+          <Segment inverted>
+              <List divided inverted relaxed>
+                  { isFetching ? (
+                  <List.Item>
+                  <Dimmer active>
+                      <Loader />
+                  </Dimmer>               
+                  </List.Item>
+                  ) : (
+                      items.books.map((item, index) => {
+                          return (
+                          <List.Item key={index}>
+                              <List.Content>
+                              <List.Header>{item.book_title}</List.Header>
+                              {item.book_author[0]}
+                              </List.Content>
+                          </List.Item> 
+                          )
+                      })
+                  )}
+              </List>
+          </Segment> 
+      </React.Fragment>)
+  };
 
-  return {
-    items,
-    isFetching,
-  }
-}
 
-export default connect(mapStateToProps)(BookList);
+
+export default BookList;
   
   
   
