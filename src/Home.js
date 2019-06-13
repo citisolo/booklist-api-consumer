7564f772582017df6c62af3726bf0882108042f3
kwadwo amankwa <citisolo@gmail.com>
2019-06-13 13:27:34 +0100

    Added pagination with styling

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,35 @@ import styled from 'styled-components';
 import BookList from './components/BookList';
 import { connect } from 'react-redux';
 import { fetchBooks } from './actions';
+import ReactPaginate from 'react-paginate';
 
 const HomeWrapper = styled.div`
     margin: 10em;
 `;
 
+const PagerWrapper = styled.div`
+    ul {
+        display: inline-block;
+    }
+
+    li {
+        display: inline-block;
+        border-style: solid;
+        padding: 0.8rem;
+        border-width: 0.1px;
+    }
+
+    li:hover {
+        background-color: blue;
+    }
+`
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             page: '1',
+            pageCount: 100,
         }
     }
 
@@ -34,6 +52,21 @@ class Home extends React.Component {
       return (
         <HomeWrapper>
             <BookList isFetching={isFetching} items={items}/>
+            <PagerWrapper>
+                <ReactPaginate 
+                    previousLabel={'previous'}
+                    nextLabel={'next'}
+                    breakLabel={'...'}
+                    breakClassName={'break-me'}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={'pagination'}
+                    subContainerClassName={'pages pagination'}
+                    activeClassName={'active'}
+                />
+            </PagerWrapper>
         </HomeWrapper>
       )
     }
